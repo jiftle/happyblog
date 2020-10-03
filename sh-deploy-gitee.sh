a4a2699544741d78265ba0cf5c325f05de824a5b
mac <mac@macbookdeMBP.lan>
2020-10-03 23:29:32 +0800

    github主页

--- sh-deploy-gitee.sh
+++ sh-deploy-gitee.sh
@@ -1,1 +1,11 @@
-hexo·clean·&&·hexo·generate·--config·_config-gitee.yml·&&·hexo·d·--config·_config-gitee.yml
+#!/bin/bash
+# -----------------------------------------------------------------
+# FileName: sh-deploy-gitee.sh
+# Date: 2020-10-03
+# Author: jiftle
+# Description: 
+# -----------------------------------------------------------------
+# hexo·clean·&&·hexo·generate·--config·_config-gitee.yml·&&·hexo·d·--config·_config-gitee.yml
+hexo·clean
+
+# hexo·generate·--config·_config-gitee.yml·&&·hexo·d·--config·_config-gitee.yml
