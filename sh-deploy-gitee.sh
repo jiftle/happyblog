f7ae44ac83689f89e21b330ec9a7751308dc3b74
mac <mac@macbookdeMBP.lan>
2020-10-06 00:05:55 +0800

    添加评价功能gitalk

--- sh-deploy-gitee.sh
+++ sh-deploy-gitee.sh
@@ -6,6 +6,17 @@
 # Description: 
 # -----------------------------------------------------------------
 # hexo·clean·&&·hexo·generate·--config·_config-gitee.yml·&&·hexo·d·--config·_config-gitee.yml
-hexo·clean
+# hexo·clean
+HexoBin='/usr/local/bin/hexo'
 
-# hexo·generate·--config·_config-gitee.yml·&&·hexo·d·--config·_config-gitee.yml
+echo "  |--> 清理"
+$HexoBin clean
+
+echo "  |--> 构建"
+HexoBin="/usr/local/bin/hexo"
+$HexoBin·g·--config·./_config-gitee.yml
+
+echo "  |--> 发布"
+HexoBin='/usr/local/bin/hexo'
+$HexoBin·d·--config·./_config-gitee.yml
+echo "  |--> 发布到Gitee完成"
